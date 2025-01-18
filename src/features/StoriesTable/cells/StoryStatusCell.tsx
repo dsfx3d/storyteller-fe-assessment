f0f0a2c86c237cc85d334d91cfd68b87c5a93428
dsfx3d <dsfx3d@gmail.com>
2025-01-18 18:16:32 +0530

    feat: enable status selection in StoryStatusCell with search parameter update

--- src/features/StoriesTable/cells/StoryStatusCell.tsx
+++ src/features/StoriesTable/cells/StoryStatusCell.tsx
@@ -1,14 +1,22 @@
 import {AStoryStatus} from "~/components/atoms/AStoryStatus";
 import {type CellContext} from "@tanstack/react-table";
+import {EFilterParams} from "~/lib/enums/EFilterParams";
 import {EStoryStatus} from "~/services/stories/enums/EStoryStatus";
 import {TStory} from "~/services/stories/types/TStory";
+import {useSearchParam} from "~/hooks/useSearchParam";
+import {useCallback} from "react";
 
 type TProps = CellContext<TStory, unknown>;
 
 export function StoryStatusCell({cell}: TProps) {
+  const status = cell.getValue<EStoryStatus>();
+  const [, setStatus] = useSearchParam(EFilterParams.Status);
+  const handleStatusClick = useCallback(() => {
+    setStatus(status);
+  }, [status, setStatus]);
   return (
-    <div className="w-full flex justify-center">
-      <AStoryStatus status={cell.getValue<EStoryStatus>()} />
+    <div className="w-full flex justify-center" onClick={handleStatusClick}>
+      <AStoryStatus status={status} />
     </div>
   );
 }
