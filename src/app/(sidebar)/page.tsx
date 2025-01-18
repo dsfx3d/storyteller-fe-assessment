9bb369f9f0c8164976551bc48f61045e7d6b0e06
dsfx3d <dsfx3d@gmail.com>
2025-01-18 15:21:58 +0530

    feat: integrate StoriesTable and fetch stories data in Home component

--- src/app/(sidebar)/page.tsx
+++ src/app/(sidebar)/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import {StoriesTable} from "~/features/StoriesTable";
+import {fetchStories} from "~/services/stories/fetchStories";
+
+export default async function Home() {
+  const data = await fetchStories();
   return (
-    <div>
-      <h1 className="text-3xl">Stories</h1>
-    </div>
+    <main className="grow bg-background lg:mb-8 overflow-hidden lg:rounded-b-2xl">
+      <h1 className="text-3xl font-black lg:rounded-b-xl px-4.5 lg:px-7.5 py-4">
+        Stories
+      </h1>
+      <StoriesTable data={data} />
+    </main>
   );
 }
