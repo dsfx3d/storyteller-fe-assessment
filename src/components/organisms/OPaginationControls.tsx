59b0706f777804f6e7c4321492576eff00eeba1d
dsfx3d <dsfx3d@gmail.com>
2025-01-18 22:26:43 +0530

    feat(OPaginationControls): bind controls with url search params

--- src/components/organisms/OPaginationControls.tsx
+++ src/components/organisms/OPaginationControls.tsx
@@ -1,112 +1,54 @@
 "use client";
 import {Button} from "../ui/button";
-import {Form, FormField} from "~/components/ui/form";
+import {EFilterParams} from "~/lib/enums/EFilterParams";
 import {Input} from "../ui/input";
 import {MoveLeft, MoveRight} from "lucide-react";
-import {Reader} from "fp-ts/lib/Reader";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../ui/select";
-import {flow} from "fp-ts/lib/function";
-import {minmax} from "~/lib/minmax";
-import {useEffect, useMemo} from "react";
-import {useForm} from "react-hook-form";
-import {z} from "zod";
-import {zodResolver} from "@hookform/resolvers/zod";
+import {OSearchFilter} from "~/components/organisms/OSearchFilter";
+import {OSelectFilter} from "~/components/organisms/OSelectFilter";
+import {SelectTrigger, SelectValue} from "../ui/select";
+import {useSearchParam} from "~/hooks/useSearchParam";
 
 type TProps = {
-  currentPage: number;
   totalPages: number;
-  onChangePage: Reader<number, void>;
 };
 
-export function OPaginationControls({
-  currentPage,
-  totalPages,
-  onChangePage,
-}: TProps) {
-  const setPage = useMemo(
-    () => flow((page: number) => minmax(page, 1, totalPages), onChangePage),
-    [onChangePage, totalPages],
-  );
-  const form = usePageForm(currentPage);
-
-  const handleSubmit = form.handleSubmit(data => {
-    setPage(+data.page);
-  }, console.error);
-
+export function OPaginationControls({totalPages}: TProps) {
+  const [page, setPage] = useSearchParam(EFilterParams.Page, {emptyValue: "1"});
   return (
-    <Form {...form}>
-      <div className="inline-flex gap-2 lg:gap-4 items-center text-sm">
-        <form
-          onSubmit={handleSubmit}
-          className="inline-flex gap-2 lg:gap-4 items-center text-sm"
-        >
-          <FormField
-            name="page"
-            control={form.control}
-            render={({field}) => (
-              <>
-                Page{" "}
-                <Input
-                  className="w-[60px] px-2 h-8"
-                  min={1}
-                  max={totalPages}
-                  {...field}
-                />
-                of {totalPages}
-              </>
-            )}
-          />{" "}
-        </form>
-        <Select>
-          <SelectTrigger className="w-[110px] h-8">
-            <SelectValue placeholder="20 Rows"></SelectValue>
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="10">20 Rows</SelectItem>
-          </SelectContent>
-        </Select>
-        <div>
-          <Button
-            role="button"
-            className="text-[#888a8f] rounded-r-none border-r-[0.5px] rounded-l w-8 h-8"
-            variant="outline"
-            onClick={() => setPage(+form.getValues("page") - 1)}
-          >
-            <MoveLeft />
-          </Button>
-          <Button
-            role="button"
-            className="text-[#888a8f] rounded-l-none border-l-[0.5px] rounded-r w-8 h-8"
-            variant="outline"
-            onClick={() => setPage(+form.getValues("page") + 1)}
-          >
-            <MoveRight />
-          </Button>
-        </div>
+    <div className="inline-flex gap-2 lg:gap-7.5 items-center text-sm">
+      <div className="flex items-center">
+        Page{" "}
+        <OSearchFilter
+          param={EFilterParams.Page}
+          as={Input}
+          className="w-[60px] px-2"
+          emptyValue="1"
+        />
+        of {totalPages}
       </div>
-    </Form>
+      <OSelectFilter param={EFilterParams.PageSize} items={[]}>
+        <SelectTrigger className="w-[110px]">
+          <SelectValue placeholder="10 Rows"></SelectValue>
+        </SelectTrigger>
+      </OSelectFilter>
+      <div>
+        <Button
+          role="button"
+          className="text-[#888a8f] rounded-r-none border-r-[0.5px] rounded-l w-8 h-8"
+          variant="outline"
+          onClick={() => setPage((+page - 1).toString())}
+        >
+          <MoveLeft />
+        </Button>
+        <Button
+          role="button"
+          className="text-[#888a8f] rounded-l-none border-l-[0.5px] rounded-r w-8 h-8"
+          variant="outline"
+          onClick={() => setPage((+page + 1).toString())}
+        >
+          <MoveRight />
+        </Button>
+      </div>
+    </div>
   );
-}
-
-const schema = z.object({
-  page: z.string(),
-});
-
-function usePageForm(currentPage: number) {
-  const form = useForm<z.infer<typeof schema>>({
-    resolver: zodResolver(schema),
-    defaultValues: {page: currentPage.toString()},
-  });
-
-  useEffect(() => {
-    form.setValue("page", currentPage.toString());
-  }, [form, currentPage]);
-
-  return form;
 }
