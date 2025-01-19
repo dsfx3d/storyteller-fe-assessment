a3cd16f19b6123537c4b9ac3853185adbaa3a910
dsfx3d <dsfx3d@gmail.com>
2025-01-19 10:21:34 +0530

    feat(Sidebar): enhance layout with CreateStory component and adjust header styling

--- src/app/(sidebar)/page.tsx
+++ src/app/(sidebar)/page.tsx
@@ -1,13 +1,15 @@
+import {CreateStory} from "~/features/CreateStory";
 import {StoriesTable} from "~/features/StoriesTable";
 import {fetchStories} from "~/services/stories/fetchStories";
 
 export default async function Home() {
   const data = await fetchStories();
   return (
-    <main className="grow bg-background lg:mb-8 overflow-hidden lg:rounded-b-2xl">
-      <h1 className="text-3xl font-black lg:rounded-b-xl px-4.5 lg:px-7.5 py-4">
-        Stories
-      </h1>
+    <main className="grow bg-background lg:mb-8.5 overflow-hidden lg:rounded-b-2xl">
+      <div className="flex justify-between px-4.5 lg:px-7.5 py-4.5">
+        <h1 className="text-3xl font-black">Stories</h1>
+        <CreateStory className="md:hidden" />
+      </div>
       <StoriesTable data={data} />
     </main>
   );
