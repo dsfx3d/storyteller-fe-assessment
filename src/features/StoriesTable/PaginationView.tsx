d86242313ff34f0b7a3287555c34d659cffe57dd
dsfx3d <dsfx3d@gmail.com>
2025-01-20 01:01:40 +0530

    feat(StoriesTable): add PaginationView component

---
+++ src/features/StoriesTable/PaginationView.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function PaginationView() {
+  return (
+    <div className="text-sm [word-spacing:_0.1rem]">Showing 1 to 10 of 100</div>
+  );
+}
--- src/features/StoriesTable/StoryFilters.tsx
+++ src/features/StoriesTable/StoryFilters.tsx
@@ -5,36 +5,40 @@ import {EStoryStatus} from "~/services/stories/enums/EStoryStatus";
 import {MSearchbar} from "~/components/molecules/MSearchbar";
 import {OSearchFilter} from "~/components/organisms/OSearchFilter";
 import {OSelectFilter} from "~/components/organisms/OSelectFilter";
+import {PaginationView} from "./PaginationView";
 import {SelectTrigger, SelectValue} from "~/components/ui/select";
 import {Suspense} from "react";
 
 export default function StoryFilters() {
   return (
-    <div className="flex gap-7.5 justify-between px-4.5 lg:px-7.5">
-      <div className="flex gap-4.5 lg:gap-7.5 grow">
-        <Suspense>
-          <OSearchFilter
-            param={EFilterParams.Search}
-            as={MSearchbar}
-            id="search-stories"
-            aria-label="Search stories"
-            className="w-full md:w-fit lg:w-[300px] xl:w-[376px]"
-          />
-          <OSelectFilter
-            clearLabel="All Statuses"
-            param={EFilterParams.Status}
-            items={Object.values(EStoryStatus)}
+    <div className="flex gap-7.5 items-center px-4.5 lg:px-7.5">
+      <Suspense>
+        <OSearchFilter
+          param={EFilterParams.Search}
+          as={MSearchbar}
+          id="search-stories"
+          aria-label="Search stories"
+          className="w-full md:w-fit lg:w-[300px] xl:w-[376px]"
+        />
+        <OSelectFilter
+          clearLabel="All Statuses"
+          param={EFilterParams.Status}
+          items={Object.values(EStoryStatus)}
+        >
+          <SelectTrigger
+            className="capitalize md:w-[200px]"
+            aria-label="Filter by status"
           >
-            <SelectTrigger
-              className="capitalize md:w-[200px]"
-              aria-label="Filter by status"
-            >
-              <SelectValue placeholder="All Statuses"></SelectValue>
-            </SelectTrigger>
-          </OSelectFilter>
-        </Suspense>
+            <SelectValue placeholder="All Statuses"></SelectValue>
+          </SelectTrigger>
+        </OSelectFilter>
+      </Suspense>
+      <div className="hidden xl:flex grow justify-end xl:justify-start">
+        <PaginationView />
       </div>
-      <CreateStory className="hidden md:flex" />
+      <div className="hidden md:flex grow xl:shrink justify-end">
+        <CreateStory />
+      </div>
     </div>
   );
 }
