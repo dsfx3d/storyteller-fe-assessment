1977dcb9673ae7e49fd1baf4452b896c1477471a
dsfx3d <dsfx3d@gmail.com>
2025-01-19 13:48:16 +0530

    refactor(fonts): remove unused Inter font import and related configuration

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,12 +1,4 @@
-import {Inter} from "next/font/google";
 import localFont from "next/font/local";
-
-export const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-inter",
-  display: "swap",
-});
 
 export const sansRegular = localFont({
   src: [
