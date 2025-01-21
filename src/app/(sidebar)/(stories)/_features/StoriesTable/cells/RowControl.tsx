494dc10b71c4284bf63e8c4fa385aaa604e2c0fb
dsfx3d <dsfx3d@gmail.com>
2025-01-21 12:56:17 +0530

    style(RowControl): adjust padding for improved layout consistency

--- src/app/(sidebar)/(stories)/_features/StoriesTable/cells/RowControl.tsx
+++ src/app/(sidebar)/(stories)/_features/StoriesTable/cells/RowControl.tsx
@@ -8,7 +8,7 @@ type TProps = CellContext<TStory, unknown>;
 export function RowActions({row}: TProps) {
   const title = row.getValue("title");
   return (
-    <div className="flex gap-4 justify-center lg:pr-2">
+    <div className="flex gap-4 justify-center lg:pr-2.5">
       <Button variant="destructive" size="sm" aria-label={`Delete ${title}`}>
         <Trash2 />
       </Button>
