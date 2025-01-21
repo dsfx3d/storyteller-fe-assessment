ea4ec23950bc6c19363fb059986d15faaf0c2734
dsfx3d <dsfx3d@gmail.com>
2025-01-21 12:22:57 +0530

    STYLE(RowControl): adjust padding for better alignment in action buttons

--- src/app/(sidebar)/(stories)/_features/StoriesTable/cells/RowControl.tsx
+++ src/app/(sidebar)/(stories)/_features/StoriesTable/cells/RowControl.tsx
@@ -8,7 +8,7 @@ type TProps = CellContext<TStory, unknown>;
 export function RowActions({row}: TProps) {
   const title = row.getValue("title");
   return (
-    <div className="flex gap-4 justify-center">
+    <div className="flex gap-4 justify-center md:pr-2.5">
       <Button variant="destructive" size="sm" aria-label={`Delete ${title}`}>
         <Trash2 />
       </Button>
