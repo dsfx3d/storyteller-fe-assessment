93e5c7964ddc4814da05c820eeeb596e0c344650
dsfx3d <dsfx3d@gmail.com>
2025-01-26 12:03:19 +0530

    feat(PaginationView): update pagination display to reflect current page and limit

--- src/app/(sidebar)/(stories)/_features/StoriesTable/PaginationView.tsx
+++ src/app/(sidebar)/(stories)/_features/StoriesTable/PaginationView.tsx
@@ -1,7 +1,14 @@
+import {EFilterParams} from "~/lib/enums/EFilterParams";
+import {useSearchParam} from "~/hooks/useSearchParam";
 import React from "react";
 
 export function PaginationView() {
+  const [page] = useSearchParam(EFilterParams.Page, {emptyValue: "1"});
+  const offset = (+page - 1) * 10;
+  const limit = offset + 10;
   return (
-    <div className="text-sm [word-spacing:_0.1rem]">Showing 1 to 10 of 100</div>
+    <div className="text-sm [word-spacing:_0.1rem]">
+      Showing {offset + 1} to {limit} of 20
+    </div>
   );
 }
